495c2ac52f18d14dd8bc7159d68da7135ad7136a
Naoto Satoh <stardustforever012@gmail.com>
2017-10-18 00:29:16 +0900

    add public to constructor

--- src/main/java/com/example/hmac/RequestParam.java
+++ src/main/java/com/example/hmac/RequestParam.java
@@ -6,14 +6,14 @@ public class RequestParam {
     String signature;
     String payload;
 
-    RequestParam(String accsess_key, int nonce, String payload,String signature) {
+    public RequestParam(String accsess_key, int nonce, String payload,String signature) {
         this.accsess_key = accsess_key;
         this.nonce = nonce;
         this.signature = signature;
         this.payload = payload;
     }
 
-    RequestParam(String accsess_key, int nonce, String payload) {
+    public RequestParam(String accsess_key, int nonce, String payload) {
         this.accsess_key = accsess_key;
         this.nonce = nonce;
         this.payload = payload;
